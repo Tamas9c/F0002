161d2acef7c1e9250d566c2d391fb0caa381aedd
Tamas9c <55052371+Tamas9c@users.noreply.github.com>
2020-10-26 12:23:42 +0100

    Add files via upload

---
+++ f0002c.py
@@ -0,0 +1,13 @@
+'''
+F0002c: Hány sornyi kiírás, azaz szöveg és üres sor kerül a képernyőre,
+ha majd egyszer lefuttatod ezt a programot?
+
+Válasz: 6 üres és 5 szöveges
+(Megjegyzés: \n olyan mint ha lenyomnád az entert egy szövegszerkesztőben.)
+'''
+
+print('Első sor\n')
+print('\nNos?')
+print('Azaz?\n')
+print('\nAkkor most mennyi? \n\n')
+print('Utolsó sor')
