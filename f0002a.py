3e4f17c8053707f8695b1e207a51a685619e6575
Tamas9c <55052371+Tamas9c@users.noreply.github.com>
2020-10-26 12:12:27 +0100

    Add files via upload

---
+++ f0002a.py
@@ -0,0 +1,22 @@
+'''
+F0002a: Melyik állat nevét nem írja ki a program?
+(Csak a válasz megadását követően ér lefuttatni a programot:))
+
+Válasz: egér
+Mivel a kutya nevű változóban van csak szövegként az egér
+és ha az egér nevű változót akarjuk kiírni akkor cicát ír ki
+mivel 'cica' az értéke.
+A futtatás után így fog kinézni amit kiírt:
+kutya
+kutya
+cica
+cica
+'''
+
+cica = 'kutya'
+kutya = 'egér'
+egér = 'cica'
+print(cica)
+print('kutya')
+print(egér)
+print('cica')
